cce515a7f50d5621203dbd80960b7b27f26d7fe5
Mathieu Morainville <mathieumorainville@hotmail.com>
2017-08-31 00:21:28 +0200

    style: add Bulma

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <navbar></navbar>
+
+    <h1 class="title">{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
@@ -20,7 +22,10 @@
 </template>
 
 <script>
+import Navbar from './Navbar.vue'
+
 export default {
+  components: {Navbar},
   name: 'hello',
   data () {
     return {
@@ -29,24 +34,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #35495E;
-}
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar has-shadow">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="http://bulma.io">
+          Vlinks
+        </a>
+
+        <div class="navbar-burger burger" data-target="c-navbar__menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div id="c-navbar__menu" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="https://github.com/jgthms/bulma" target="_blank">
+            About
+          </a>
+        </div>
+
+        <div class="navbar-end">
+          <a class="navbar-item" href="https://github.com/jgthms/bulma" target="_blank">
+            Login
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'navbar'
+  }
+</script>
